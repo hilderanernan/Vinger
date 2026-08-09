7925a14b02a0928b67820c7698b676bacd58c821
hilderanernan <hilderanernan574@gmail.com>
2026-08-09 11:22:33 +0700

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-4">
+      <h1 className="text-5xl font-bold text-primary">Vinger</h1>
+      <p className="mt-4 text-text/70">Platform sosial berbasis audio pendek</p>
+    </main>
+  );
+}
